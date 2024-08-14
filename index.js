ac2c9b226e9efd2187192c46af33ecea33ea90d3
rs-ranjit <ranjitrahul7@gmail.com>
2024-08-14 18:26:01 +0545

    starting

---
+++ index.js
@@ -0,0 +1,9 @@
+let inputBtn = document.getElementById("input-btn");
+let inputEl = document.getElementById("input-el");
+
+let myLeads = [];
+
+
+inputBtn.addEventListener("click", ()=>{
+    console.log("The button has been clicked");
+});

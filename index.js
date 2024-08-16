ff973279f77448fea3c585bd5c31c7479d101d8f
rs-ranjit <ranjitrahul7@gmail.com>
2024-08-16 12:23:39 +0545

    almost done

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
+const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
 
-let myLeads = ["rahul", "sultan", "ranjit"];
+let myLeads = [];
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    renderLeads();
+}
+
+deleteBtn.addEventListener("dblclick", ()=>{
+    localStorage.clear();
+    myLeads = [];
+    renderLeads();
+});
 
 inputBtn.addEventListener("click", ()=>{
     myLeads.push(inputEl.value);
-    console.log(myLeads);
+    inputEl.value = "";
+    localStorage.setItem("myLeads",JSON.stringify(myLeads));
+    renderLeads();
+
 });
 
-        let listItems = myLeads.map(lead => `<li>${lead}</li>`).join('');
-        ulEl.innerHTML = listItems;
+function renderLeads(){
+    let listItems = myLeads.map(lead => `<li><a href="${lead}" target="_blank">${lead}</a></li>`).join('');
+    ulEl.innerHTML = listItems;
+
+//alternative of using map
+// for(i=0;i<myLeads.length;i++){
+//     const li = document.createElement("li")
+//     li.textContent = myLeads[i];
+//     ulEl.append(li);
+// }
+
+//another alternative
+
+// let listItems = "";
+// for(i=0;i<myLeads.length;i++){
+//     listItems += "<li>" + myLeads[i] + "</li>";
+//     console.log(typeof(listItems));
+// }
+//  ulEl.innerHTML = listItems;
+}
+
 

796bd78ec565e94e229c0bc87bf45dd20d845aed
rs-ranjit <ranjitrahul7@gmail.com>
2024-08-16 17:44:37 +0545

    The complete chrome extension using JS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 const ulEl = document.getElementById("ul-el");
 
 let myLeads = [];
@@ -8,25 +9,33 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if(leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
-    renderLeads();
+    renderLeads(myLeads);
 }
+
+tabBtn.addEventListener("click", ()=>{
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs){
+            myLeads.push(tabs[0].url);
+            localStorage.setItem("myLeads", JSON.stringify(myLeads));
+            renderLeads(myLeads);
+    });
+});
 
 deleteBtn.addEventListener("dblclick", ()=>{
     localStorage.clear();
     myLeads = [];
-    renderLeads();
+    renderLeads(myLeads);
 });
 
 inputBtn.addEventListener("click", ()=>{
     myLeads.push(inputEl.value);
     inputEl.value = "";
     localStorage.setItem("myLeads",JSON.stringify(myLeads));
-    renderLeads();
+    renderLeads(myLeads);
 
 });
 
-function renderLeads(){
-    let listItems = myLeads.map(lead => `<li><a href="${lead}" target="_blank">${lead}</a></li>`).join('');
+function renderLeads(leads){
+    let listItems = leads.map(lead => `<li><a href="${lead}" target="_blank">${lead}</a></li>`).join('');
     ulEl.innerHTML = listItems;
 
 //alternative of using map
